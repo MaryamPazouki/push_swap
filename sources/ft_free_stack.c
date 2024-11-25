7b48c4f69a28afa6c4eecc583953a7cf943e6f2b
MaryamPazouki <maryam.pazoki@gmail.com>
2024-11-25 10:13:50 +0000

    ft_free done

---
+++ sources/ft_free_stack.c
@@ -0,0 +1,19 @@
+#include "push_swap.h"
+
+void ft_free_stack(t_list **stack)
+{
+    t_list *current;
+    t_list *next;
+
+    if (stack == NULL || *stack == NULL)
+        return;
+    
+    current = *stack;
+    while(current)
+    {
+        next = current ->next;
+        free(current);
+        current = next;
+    }
+    *stack = NULL;
+}
