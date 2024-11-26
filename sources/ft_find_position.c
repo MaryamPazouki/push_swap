09159fff3ae2f272b380ab609cc8baf18ba62b64
MaryamPazouki <maryam.pazoki@gmail.com>
2024-11-26 00:27:54 +0000

    min max

--- sources/ft_find_position.c
+++ sources/ft_find_position.c
@@ -1,7 +1,7 @@
 #include "push_swap.h"
 
 
-int ft_find_pos(t_list *stack, int nbr)
+int ft_find_position(t_list *stack, int nbr)
 {
     int i = 0;
 
@@ -18,9 +18,52 @@ int ft_find_pos(t_list *stack, int nbr)
     return -1;
 }
 
+// Find the position for nbr_push in the stack, based on stack_b logic
+int ft_find_place_b(t_stack *stack_b, int push_value) {
+    if (!stack_b)
+        return -1;
 
+    t_stack *current = stack_b;
+    t_stack *next = stack_b->next;
+    int i = 0;
 
+    if (push_value > ft_max(stack_b) || push_value < ft_min(stack_b)) {
+        // Place after the maximum value
+        return ft_find_position(stack_b, ft_max(stack_b));
+    }
 
-// This function finds the correct place of the number in stack_b.
-// In other words, it check what index number nbr_push will get 
-// after it is being pushed to the stack_b.
+    while (current && next) {
+        if (current->content < push_value && next->content > push_value)
+            return i + 1; // Return the index of the next node
+        current = next;
+        next = next->next;
+        i++;
+    }
+    return 0; // Default to placing at the top
+}
+
+// Find the position for nbr_push in the stack, based on stack_a logic
+int ft_find_place_a(t_stack *stack_a, int push_value) 
+{
+    if (!stack_a)
+        return -1;
+
+    t_stack *current = stack_a;
+    t_stack *next = stack_a->next;
+    int i = 0;
+
+    if (push_value > ft_max(stack_a) || push_value < ft_min(stack_a)) {
+        // Place before the minimum value
+        return ft_find_position(stack_a, ft_min(stack_a));
+    }
+
+    while (current && next)
+    {
+        if (current->nbr > push_value && next->nbr < push_value)
+            return i + 1; // Return the index of the next node
+        current = next;
+        next = next->next;
+        i++;
+    }
+    return 0; // Default to placing at the top
+}
