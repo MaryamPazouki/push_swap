5cd5111cd0751e3a435e4f54b9ce525d468e4f6d
MaryamPazouki <maryam.pazoki@gmail.com>
2024-11-29 11:08:54 +0000

    sort function

--- sources/ft_sort_main.c
+++ sources/ft_sort_main.c
@@ -5,7 +5,8 @@ void ft_push_alltill3_ab(t_list **a, t_list **b)
     int size_a;
 
     size_a = ft_lstsize(*a);
-    while (size_a > 3) {
+    while (size_a > 3) 
+    {
         ft_push_to_b(a, b, 1); // Push from stack_a to stack_b
         size_a--;
     }
@@ -15,7 +16,8 @@ void ft_sort_a(t_list **a, t_list **b) {
     t_list *temp;
     int opt_cost;
 
-    while (*b) { // Continue until stack_b is empty
+    while (*b) 
+    { // Continue until stack_b is empty
         temp = *b;
         opt_cost = ft_optimal_cost(*a, *b);
 
@@ -30,25 +32,33 @@ void ft_sort_a(t_list **a, t_list **b) {
     }
 }
 
-void ft_last_sort_a(t_list **a) {
+void ft_last_sort_a(t_list **a) 
+{
     int i;
 
     i = ft_find_position(*a, ft_min(*a));
-    if (i < ft_lstsize(*a) / 2) {
+    if (i < ft_lstsize(*a) / 2) 
+    {
         while ((*a)->content != ft_min(*a))
             ft_ra(a, 0);
-    } else {
+    } 
+    else 
+    {
         while ((*a)->content != ft_min(*a))
             ft_rra(a, 0);
     }
 }
 
-void ft_sort(t_list **stack_a) {
+void ft_sort(t_list **stack_a) 
+{
     t_list *stack_b = NULL;
 
-    if (ft_lstsize(*stack_a) == 2) {
+    if (ft_lstsize(*stack_a) == 2) 
+    {
         ft_sa(stack_a, 0);
-    } else {
+    } 
+    else 
+    {
         ft_push_alltill3_ab(stack_a, &stack_b); // Pass both stacks
         ft_sort_a(stack_a, &stack_b);          // Sort the stacks
         ft_last_sort_a(stack_a);               // Final adjustment
